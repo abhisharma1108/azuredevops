51b8b98543b191bc1a764abb26cd00ddd7c33e8a
Abhishek <abhisharma1108@yahoo.co.in>
2025-02-21 01:08:57 +0530

    readytoazure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azuredevops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
